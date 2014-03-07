0532053142457ad8daceeefac9d64dfba0052850
morkion <morkion@gmail.com>
2014-03-08 00:29:02 +0200

    Inventory script

---
+++ Assets/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour 
+{
+	string[] items;
+	void Start()
+	{
+		items = new string[8];
+	}
+
+	void Update()
+	{
+
+	}
+
+	void AddItem(string item)
+	{
+		for(int i = 0; i < items.Length; i++){
+			if(items[i]==null||items[i]==""){
+				items[i] = item;
+				break;
+			}
+		}
+	}
+
+	void DropItem(int slot)
+	{
+		items[slot] = "";
+	}
+}
