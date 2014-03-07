8919243f81922cc76dacfe4e2be4cb137c7a8f91
morkion <morkion@gmail.com>
2014-03-08 00:35:18 +0200

    Random loot script and organised assets a bit

---
+++ Assets/Scripts/RandomLoot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomLoot : MonoBehaviour 
+{
+	public string[] loot;
+	string chosenLoot;
+	void Start()
+	{
+		chosenLoot = loot[Random.Range(0,loot.Length)];
+	}
+
+	void TakeLoot(GameObject player)
+	{
+		player.BroadcastMessage("AddItem", chosenLoot, SendMessageOptions.RequireReceiver);
+	}
+
+}
